2f01f8a103e81eb95da1bed8ca983f41bd6f5730
KRISHNA KUMAR VERMA <155811536+wd369018@users.noreply.github.com>
2026-03-12 21:40:48 +0530

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,22 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number"<<endl;
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         cout<<i<<"\t";
+//     }
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << "apna college" << endl;
+    }
+    return 0;
+}
