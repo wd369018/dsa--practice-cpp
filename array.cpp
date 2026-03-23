d78e4433aa3af114afd115e2df9794023e1c267d
KRISHNA KUMAR VERMA <155811536+wd369018@users.noreply.github.com>
2026-03-23 17:27:06 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int marks[10]={1,2,3,45,5,3,2,3,4,5};
+   for (int i = 0; i < 10; i++)
+   {
+    cout<<marks[i]<<" ";
+   }
+   
+return 0;
+}
