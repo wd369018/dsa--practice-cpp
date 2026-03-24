06d57df9e58ddc31224e0b7789fa4a4922f670d8
KRISHNA KUMAR VERMA <155811536+wd369018@users.noreply.github.com>
2026-03-24 18:28:58 +0530

    Add files via upload

--- loop.cpp
+++ loop.cpp
@@ -10,13 +10,59 @@
 //     return 0;
 // }
 
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cout<<"enter the number"<<endl;
+//     cin>>n;
+//     int sum=0;
+//     int i=1;
+//     while(i<=n){
+//         sum+=i;
+//         i++;
+//     }
+//     cout<<sum;
+// } 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     typedef long long ll;
+//     ll r,n=113,sum=0;
+//     while(n>0){
+//         r=n%10;
+//         sum=sum*10+r;
+//         n/=10;
+//     }
+//     cout<<sum;
+    
+// return 0;
+// }
+
+#include<iostream>
+#include<cmath>
 using namespace std;
-int main()
-{
-    for (int i = 1; i <= 5; i++)
+int main(){
+    int n;
+    cin>>n;
+    if (n<2)
     {
-        cout << "apna college" << endl;
+        cout<<"not prime";
     }
-    return 0;
+    else{
+        int i=sqrt(n);
+        
+        while(i*i<=n){
+           if(n%i==0){
+            cout<<"not prime";
+            return 0;
+           }
+         i++;
+        }
+        cout<<"prime";
+    
+     
+    }
 }
