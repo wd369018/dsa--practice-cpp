beedb073f8723967355027b52f4fd61403fcc264
KRISHNA KUMAR VERMA <155811536+wd369018@users.noreply.github.com>
2026-03-25 16:22:45 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,95 @@
+// #include <iostream>
+// #define X 25
+// using namespace std;
+// int main()
+// {
+//     const float PI = 3.14;
+//     int r;
+//     cout << "enter the number";
+//     cin >> r;
+//     cout << PI * r * r<<endl;
+//     cout<<X*r;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << (bool)3 + 2 << endl;
+//     cout << (23.5 + 2 + 'A');
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float a=2,b=4;
+//     cout<<a+b<<endl;
+//     cout<<a*b<<endl;
+//     cout<<a-b<<endl;
+//     cout<<a/b<<endl;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=2,b=4;
+//     a=++a;//a++
+//     b=--b;//b++
+//     cout<<a<<"\t"<<b<<endl;
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 12;
+//     a += 5;
+//     cout << a<<endl;
+//     a -= 5;
+//     cout << a<<endl;
+//     a *= 5;
+//     cout << a<<endl;
+//     a /= 5;
+//     cout<<a<<endl;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=3,b=4;
+//     cout<<(a>b)<<endl;
+//     cout<<(a<b)<<endl;
+//     cout<<(a>=b)<<endl;
+//     cout<<(a<=b)<<endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 3, b = 3;
+//     cout << (a == b) << endl;
+//     cout << (a != b) << endl;
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=5,b=6;
+    if(a>b && a<b){
+        cout<<"True";
+    }
+    else{
+        cout<<"false";
+    }
+    return 0;
+}
